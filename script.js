1acc20b0c606de4fdd9fefb7c8863e79ff71e2a5
TMMDSS <souzatailam@gmail.com>
2023-03-27 22:57:17 -0300

    adcionei js

---
+++ script.js
@@ -0,0 +1,13 @@
+/*function entrar() {
+    var logo = document.getElementById('logoLaranja')
+    logo.src = 'imagens/crunchyroll-logo-branco.png' 
+}*/
+
+ function entrar() {
+  var imagem = document.getElementById("logoLaranja");
+  if (imagem.src.match("imagens/crunchyroll-logo.png")) {
+    imagem.src = "imagens/crunchyroll-logo-branco.png";
+  } else {
+    imagem.src = "imagens/crunchyroll-logo.png";
+  }
+}
